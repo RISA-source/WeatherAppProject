aa001c3aeed777cf0fdbf1389f4c511950360a61
RISA-source <rishisaraffofficial@gmail.com>
2025-02-04 19:17:12 +0545

    Version-1.0

---
+++ 2501941_rishiSaraff__script_prototype2.js
@@ -0,0 +1,103 @@
+// API Key
+const api_key = '5f4671bf9bcb211342029ffcd3ef261c';
+
+// Default City
+const defaultCity = 'Opelika';
+let firstRun = true;
+// Running the code initially. We see the data of defaultCity.
+if(firstRun){
+    fetchWeatherData(defaultCity);
+    firstRun = false;
+}
+
+// Getting the Elements
+const elements = {
+    searchButton : document.getElementById('search-button'),
+    cityInput : document.getElementById('city'),
+    cityName : document.getElementById('city-name'),
+    mainWeather : document.getElementById('main-weather'),
+    weatherCondition : document.getElementById('weather-condition'),
+    weatherImage : document.getElementById('weather-image'),
+    dateElement : document.getElementById('day-date'),
+    pressureElement : document.getElementById('pressure-data'),
+    humidityElement : document.getElementById('humidity-data'),
+    windspeedElement : document.getElementById('wind-speed-data'),
+    degreeElement : document.getElementById('degree-celsius')
+}
+
+// Date Day Function
+function date_day(){
+    // Current Date & Date
+    const todayDate = new Date()
+
+    const weekDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    const day = weekDays[todayDate.getDay()]; // Day
+
+    const date = todayDate.getDate(); // Date
+    const month = months[todayDate.getMonth()]; // Month 
+    const year = todayDate.getFullYear(); // Year
+    
+    // Display in Day, date / month / year format.
+    elements.dateElement.innerText = `${day}, ${month} ${date} / ${year}`;
+}
+
+// Calling the function for Date and Day.
+date_day();
+
+// Checking for Click on Search for City
+elements.searchButton.addEventListener('click',function(){
+    console.log('Search Button Clicked');
+    const city = elements.cityInput.value; // Taking the value from searchbar
+    if (city) {
+        // Call the function to fetch the weather data
+        console.log('Fetching Data...');
+        fetchWeatherData(city);
+    }else{
+        alert('ERROR: Please Enter a City Name')
+        throw new Error('Empty Search | No City Name Found!')
+    }
+})
+
+async function fetchWeatherData(city) {
+    // URL for API
+    const geo_url = `http://localhost/php-scripts/Prototype2/2501941_rishiSaraff__weather_prototype2.php?t=${city}`;
+
+    // Fetching data from API
+    const response = await fetch(geo_url);
+
+    if (!response.ok){
+        alert('ERROR: Please Give Correct City Name or Check Your Internet connection.')
+        throw new Error(`${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log('Data Fetched and Converted.');
+    console.log(data)
+
+    // // Storing necessary data
+    const country = data[0].Country;
+    const place = data[0].City;
+    const main = data[0].Main_Weather;
+    let description = data[0].Weather_Description;
+    const iconCode = data[0].Icon_Code;
+    const icon = `https://openweathermap.org/img/wn/${iconCode}@4x.png`
+    const pressure = data[0].Pressure;
+    const windSpeed = data[0].Wind_Speed;
+    const humidity = data[0].Humidity;
+    const degree = data[0].Temp_Degree;
+    const direction = data[0].Direction;
+
+    // Converting first letter of description to Capital
+    description = description.charAt(0).toUpperCase() + description.slice(1);
+
+    // Populating the DOM
+    elements.cityName.innerText = `${place}, ${country}`;
+    elements.mainWeather.innerText = `${main}`;
+    elements.weatherCondition.innerText = `${description}`;
+    elements.weatherImage.src = icon
+    elements.pressureElement.innerText = `${pressure} hPa`;
+    elements.humidityElement.innerText = `${humidity} %`;
+    elements.windspeedElement.innerText = `${windSpeed} m/s  ${direction}°`;
+    elements.degreeElement.innerText = `${degree}°C`;
+}
---
+++ 2501941_rishiSaraff__weather_prototype2.php
@@ -0,0 +1,141 @@
+<?php
+
+# Connection Establishing
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$conn = mysqli_connect($host,$username,$password);
+if ($conn){
+}else{
+    echo 'Connection Unsuccessful '.mysqli_connect_error();
+    die('Error');
+}
+# Connection Established
+
+#Creating the database
+$sql_db = "CREATE DATABASE IF NOT EXISTS WeatherForecast;";
+$result = mysqli_query($conn,$sql_db);
+if ($result){
+}else{
+    echo 'Failed to create database '.mysqli_connect_error();
+}
+
+# Selecting the created database
+mysqli_select_db($conn, 'WeatherForecast');
+ 
+# Creating the table
+$city_table = 'CREATE TABLE IF NOT EXISTS Cities(
+City varchar(255) primary key,
+Country varchar(255),
+Main_Weather varchar(255),
+Weather_Description varchar(255),
+Temp_Degree decimal(10,2),
+Icon_Code varchar(100),
+Pressure decimal(10,2),
+Wind_Speed decimal(10,2),
+Humidity decimal(10,2),
+Direction decimal(10,2),
+Get_Time DATETIME DEFAULT CURRENT_TIMESTAMP 
+);';
+
+# Putting the changes to the MySQL
+$result1 = mysqli_query($conn,$city_table);
+# Checking for errors in SQL
+if($result1){
+}else{
+    echo 'table not created '.mysqli_connect_error();
+}
+
+# Getting the movie name from the Client Side.
+if(isset($_GET['t'])){
+    $city = $_GET['t'];
+}else{
+    $city = 'Opelika';
+}
+
+function fetchdata($conn,$city){
+    # OMDB URL
+    $api_key = '5f4671bf9bcb211342029ffcd3ef261c';
+    $url = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$api_key&units=metric";
+
+    # Fetching the data from the API
+    $citydata = file_get_contents($url);
+    $data = json_decode($citydata, True);
+
+    # Now taking the necessary data
+    $city_name = $data['name'];
+    $country = $data['sys']['country'];
+    $mainWeather = $data['weather'][0]['main'];
+    $weather_desc = $data['weather'][0]['description'];
+    $temp_degree = $data['main']['temp'];
+    $iconCode = $data['weather'][0]['icon'];
+    $pressure = $data['main']['pressure'];
+    $windSpeed = $data['wind']['speed'];
+    $humidity = $data['main']['humidity'];
+    $direction = $data['wind']['deg'];
+
+    # Storing the data in the SQL database
+    $sql_insert = "INSERT INTO Cities(City, Country,Main_Weather,Weather_Description,Temp_Degree,Icon_Code,Pressure,Wind_Speed,Humidity,Direction) Values('$city_name','$country','$mainWeather','$weather_desc','$temp_degree','$iconCode','$pressure','$windSpeed','$humidity','$direction')";
+    # Checking for any errors while entring the data in the database   
+    if(mysqli_query($conn,$sql_insert)){
+    }else{
+        echo 'Record not Inserted '.mysqli_connect_error();
+    }
+    $sql_select1 = "SELECT * FROM Cities WHERE City = '$city_name';";
+    # Getting the data from MySQL
+    $result4 = mysqli_query($conn,$sql_select1);
+    # Checking for errors in SQL
+    if($result4){
+    }else{
+        echo 'Data not fetched from database '.mysqli_connect_error();
+    }
+    $reqData[] = $result4;
+    return $reqData;
+}
+
+# Selecting all the data of the SQL database
+$sql_select = 'SELECT * FROM Cities;';
+$sql_delete = "DELETE FROM Cities WHERE City = '$city';";
+# Getting the data from MySQL
+$result2 = mysqli_query($conn,$sql_select);
+# Checking for errors in SQL
+if($result2){
+}else{
+    echo 'Data not fetched from database '.mysqli_connect_error();
+}
+
+# Checking if data already exists in the table
+if (mysqli_num_rows($result2)==0){
+    $finaldata[] = fetchdata($conn, $city);
+}else{
+    while($row = mysqli_fetch_assoc($result2)){
+        if(strtolower($row['City'])==strtolower($city)){
+            $lastFetchTime = new DateTime($row['Get_Time']);
+            $currentTime = new DateTime();
+            $timeDiff = $currentTime->getTimestamp() - $lastFetchTime->getTimestamp();
+
+            if ($timeDiff > 2 * 60 * 60) { // 2 hours in seconds
+                $result8 = mysqli_query($conn,$sql_delete);
+                # Checking for errors in SQL
+                if($result8){
+                }else{
+                    echo 'Data not able to delete!'.mysqli_connect_error();
+                // Fetch new data and update fetch_time
+                $finaldata = fetchdata($conn, $city);
+                }
+            } else {
+                // Use existing data
+                $finaldata[] = $row ;
+            }
+        }
+    }
+    if ($finaldata == []){
+        $finaldata = fetchdata($conn, $city);
+    }
+}
+
+$tobesent = json_encode($finaldata);
+header('content-type: application/json');
+echo $tobesent
+
+?>
